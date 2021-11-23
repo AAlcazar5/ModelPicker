73135ea8d810a22a2f70f0edd9fe5521f236e57f
Alex <aalcazar064@gmail.com>
2021-11-23 15:35:08 -0800

    Initial Commit

---
+++ ModelPicker/ModelPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModelPickerApp.swift
+//  ModelPicker
+//
+//  Created by Alejandro Alcazar on 11/23/21.
+//
+
+import SwiftUI
+
+@main
+struct ModelPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
